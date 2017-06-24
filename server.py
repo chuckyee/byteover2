73eb75d9761311d05b6c44da79b12322bfb984e8
chuckyee <chuckyee@physics.rutgers.edu>
2017-06-24 16:49:36 +0000

    Add stub function for sentiment analysis.

--- server.py
+++ server.py
@@ -4,6 +4,11 @@ import json
 
 app = Flask(__name__)
 
+
+def analyze_sentiment(text):
+    """Analyze sentiment of passed string"""
+    return 0
+
 @app.route('/', methods=['GET'])
 def handle_get():
     pass
@@ -13,6 +18,9 @@ def handle_sms():
     """Respond to incoming SMS with a simple text message."""
     print(request.values)
     body = request.values.get("Body")
+
+    sentiment = analyze_sentiment(body)
+    print(sentiment)
 
     text = "Puny human, '{} in bed with cats' to you too.".format(body)
     resp = MessagingResponse().message(text)
