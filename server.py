b7e3cb4edaa80b29311d0f107f0643ad3dc121c3
chuckyee <chuckyee@physics.rutgers.edu>
2017-06-24 23:07:42 +0000

    Handle voice calls and write out mp3 recording of user's voice to file.

--- server.py
+++ server.py
@@ -1,7 +1,15 @@
-from flask import Flask, request
-from twilio.twiml.messaging_response import MessagingResponse
+import requests
 import json
+import io
+import shutil
+from flask import Flask, request
+
+from twilio.twiml.messaging_response import MessagingResponse
+from twilio.twiml.voice_response import Record, VoiceResponse
+from twilio.rest import Client
+
 from azure import askazure
+
 
 app = Flask(__name__)
 
@@ -16,14 +24,27 @@ def analyze_sentiment(text):
     score = sentiment['documents'][0]['score']
     return score
 
-@app.route('/', methods=['GET'])
-def handle_get():
-    pass
+@app.route('/record_action', methods=['POST'])
+def handle_record_action():
+    print("Handling record action callback.")
+    url = request.form.get("RecordingUrl") + ".mp3"
+    filename = "/tmp/{}".format(url.split('/')[-1]) # get unique filename
+    r = requests.get(url)
+    with open(filename, 'wb') as f:
+        shutil.copyfileobj(io.BytesIO(r.content), f)
+        print("Wrote mp3 out to file {}".format(filename))
+    return "Hello"
 
 @app.route('/voice', methods=['GET', 'POST'])
 def handle_voice():
     """Handle incoming voice calls."""
-    pass
+    response = VoiceResponse()
+    response.say("Hello human.")
+    response.record(timeout=3, transcribe=True, action='/record_action')
+    response.say("Thanks. Goodbye.")
+    response.hangup()
+    print(response)
+    return str(response)
 
 @app.route('/sms', methods=['GET', 'POST'])
 def handle_sms():
@@ -41,3 +62,6 @@ def handle_sms():
 if __name__ == '__main__':
     app.config.from_pyfile('config')
     app.run(port=int(app.config['PORT']), debug=True)
+    #account_sid = "ACc1ae6b931a01753024331e505977abcc"
+    #auth_token = 
+    client = Client(account_sid, auth_token)
