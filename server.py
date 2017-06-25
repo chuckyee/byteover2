191892df27f054834dddfcde4ba91d84c4f84e5e
chuckyee <chuckyee@physics.rutgers.edu>
2017-06-25 04:12:52 +0000

    Full integration with Twilio recording, Azure speech to text, Azure text sentiment, audio mixing and Twilio audio playback to user.

--- server.py
+++ server.py
@@ -2,25 +2,23 @@ import requests
 import json
 import io
 import shutil
-from flask import Flask, Response, request
+import time
 
+from flask import Flask, Response, request
 from twilio.twiml.messaging_response import MessagingResponse
 from twilio.twiml.voice_response import Record, VoiceResponse
 
-from azure import askazure
+import azure
 import audio
-
 
 app = Flask(__name__)
 
 
 def analyze_sentiment(text):
     """Analyze sentiment of passed string
-
-    Returns: json Azure sentiment object
     """
     key = app.config['AZURE_SUBSCRIPTION_KEY']
-    sentiment = askazure.askazure(text, key)
+    sentiment = azure.text_to_sentiment(text, key)
     score = sentiment['documents'][0]['score']
     return score
 
@@ -67,27 +65,47 @@ def handle_recording_status():
 @app.route('/record_action', methods=['POST'])
 def handle_record_action():
     print("Handling record action callback.")
-    response = VoiceResponse()
-    response.say("Please wait.")
-    return str(response)
-    # audio_format = ".mp3"
-    # url = request.form.get("RecordingUrl")
-    # basename = url.split('/')[-1] # get unique filename
-    # filename = "/tmp/{}{}".format(basename, audio_format)
-    # r = requests.get(url + audio_format)
-    # with open(filename, 'wb') as f:
-    #     shutil.copyfileobj(io.BytesIO(r.content), f)
-    #     print("Wrote mp3 out to file {}".format(filename))
 
-    # filename_happy_music = './music/brandenburg.wav'
-    # outputfile = "/tmp/response_{}.wav".format(basename)
-    # audio.mixing_librosa(filename, filename_happy_music, outputfile)
-    # audio_endpoint = "{}/music/{}".format(app.config['PUBLIC_URL'], basename)
-    # response = VoiceResponse()
-    # response.play(audio_endpoint)
-    # response.say("Thanks. Goodbye.")
-    # response.hangup()
-    # return str(response)
+    # Generate temporary filename to store mp3 recording
+    audio_format = ".mp3"
+    url = request.form.get("RecordingUrl")
+    basename = url.split('/')[-1] # get unique filename
+    mp3_filename = "/tmp/{}{}".format(basename, audio_format)
+
+    # download recording from Twilio and write to file
+    time.sleep(0.5)
+    r = requests.get(url + audio_format)
+    with open(mp3_filename, 'wb') as f:
+        shutil.copyfileobj(io.BytesIO(r.content), f)
+        print("Wrote mp3 out to file {}".format(mp3_filename))
+
+    # convert mp3 file to wav file, then use Azure's speech to text
+    wav_filename = "/tmp/{}.wav".format(basename)
+    audio.mp3_to_wav(mp3_filename, wav_filename)
+    text = azure.wav_to_text(wav_filename, app.config['AZURE_REQUEST_ID'], app.config['AZURE_JWT'])
+    print("Azure speech to text returned: {}".format(text))
+
+    # analyze sentiment
+    sentiment = analyze_sentiment(text)
+    print("Sentiment score is: {}".format(sentiment))
+
+    # select music based on sentiment
+    if sentiment <= 0.5:
+        background_music = './music/imperial-march-quieter.wav'
+    else:
+        background_music = './music/brandenburg.wav'
+
+    # mix background music into call audio
+    outputfile = "/tmp/response_{}.wav".format(basename)
+    audio.mixing_librosa(wav_filename, background_music, outputfile)
+
+    # setup response to play file
+    audio_endpoint = "{}/music/{}".format(app.config['PUBLIC_URL'], basename)
+    response = VoiceResponse()
+    response.play(audio_endpoint)
+    response.say("Thanks. Goodbye.")
+    response.hangup()
+    return str(response)
 
 
 @app.route('/voice', methods=['GET', 'POST'])
