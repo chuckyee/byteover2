2784f9f331a0769ae93fd8e5834e4e68e7175867
chuckyee <chuckyee@physics.rutgers.edu>
2017-06-25 02:43:36 +0000

    Implement audio mixing with Bach Brandenburg concerto and serve wav file back to Twilio.

--- server.py
+++ server.py
@@ -2,13 +2,13 @@ import requests
 import json
 import io
 import shutil
-from flask import Flask, request
+from flask import Flask, Response, request
 
 from twilio.twiml.messaging_response import MessagingResponse
 from twilio.twiml.voice_response import Record, VoiceResponse
-from twilio.rest import Client
 
 from azure import askazure
+import audio
 
 
 app = Flask(__name__)
@@ -24,27 +24,55 @@ def analyze_sentiment(text):
     score = sentiment['documents'][0]['score']
     return score
 
+
+@app.route('/transcribe_action', methods=['POST'])
+def handle_transcribe_action():
+    print("Handling transcribe action callback")
+    text = request.form.get("TranscriptionText")
+    return 0
+
+
+@app.route('/music/<audiofile>', methods=['GET'])
+def handle_audiofile_get(audiofile):
+    filename = "/tmp/response_{}.wav".format(audiofile)
+    print("Serving file {}".format(filename))
+    with open(filename, 'rb') as f:
+        audio_data = f.read()
+    return Response(audio_data, mimetype="audio/wav")
+
+
 @app.route('/record_action', methods=['POST'])
 def handle_record_action():
     print("Handling record action callback.")
-    url = request.form.get("RecordingUrl") + ".mp3"
-    filename = "/tmp/{}".format(url.split('/')[-1]) # get unique filename
-    r = requests.get(url)
+    audio_format = ".mp3"
+    url = request.form.get("RecordingUrl")
+    basename = url.split('/')[-1] # get unique filename
+    filename = "/tmp/{}{}".format(basename, audio_format)
+    r = requests.get(url + audio_format)
     with open(filename, 'wb') as f:
         shutil.copyfileobj(io.BytesIO(r.content), f)
         print("Wrote mp3 out to file {}".format(filename))
-    return "Hello"
+
+    filename_happy_music = './music/brandenburg.wav'
+    outputfile = "/tmp/response_{}.wav".format(basename)
+    audio.mixing_librosa(filename, filename_happy_music, outputfile)
+    audio_endpoint = "{}/music/{}".format(app.config['PUBLIC_URL'], basename)
+    response = VoiceResponse()
+    response.play(audio_endpoint)
+    response.say("Thanks. Goodbye.")
+    response.hangup()
+    return str(response)
+
 
 @app.route('/voice', methods=['GET', 'POST'])
 def handle_voice():
     """Handle incoming voice calls."""
     response = VoiceResponse()
-    response.say("Hello human.")
-    response.record(timeout=3, transcribe=True, action='/record_action')
-    response.say("Thanks. Goodbye.")
-    response.hangup()
+    response.say("Record a message and stay on the line when finished.")
+    response.record(timeout=2, action='/record_action')
     print(response)
     return str(response)
+
 
 @app.route('/sms', methods=['GET', 'POST'])
 def handle_sms():
@@ -55,13 +83,18 @@ def handle_sms():
     score = analyze_sentiment(body)
     print(score)
 
-    text = "Puny human, '{} in bed with cats' to you too. Your level of positivity is {}".format(body, score)
-    resp = MessagingResponse().message(text)
+    if score < 0.33333:
+        text = "Dude, lighten the f- up."
+    elif score < 0.66666:
+        text = "You seem neither here nor there."
+    else:
+        text = "Stop farting rainbows and unicorns."
+
+    resp = MessagingResponse()
+    resp.message(text)
     return str(resp)
+
 
 if __name__ == '__main__':
     app.config.from_pyfile('config')
     app.run(port=int(app.config['PORT']), debug=True)
-    #account_sid = "ACc1ae6b931a01753024331e505977abcc"
-    #auth_token = 
-    client = Client(account_sid, auth_token)
